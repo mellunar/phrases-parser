69fc4889737bb72fdea89a6abe792b48428a9b1b
Melissa Fernandes <melfer100@gmail.com>
2022-10-25 09:29:18 -0300

    feat: improve UX & UI

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -2,9 +2,17 @@
   let finalObject = {};
   let parsed, unparsed, finalJson;
 
+  let copied = false;
+  let failed = false;
+  let invalid = false;
+
   function parseJson(input) {
     const { value } = input.target;
     unparsed = value;
+
+    if (input?.key === "Enter" || input?.keyCode === 13) {
+      submit();
+    }
   }
 
   function stringToObj(str) {
@@ -24,34 +32,89 @@
       return;
     }
 
-    parsed = JSON.parse(unparsed);
+    try {
+      parsed = JSON.parse(unparsed);
 
-    const keys = Object.keys(parsed);
+      const keys = Object.keys(parsed);
 
-    keys.forEach((key) => {
-      const newObject = stringToObj(key);
+      keys.forEach((key) => {
+        const newObject = stringToObj(key);
 
-      const newKeys = Object.keys(newObject);
+        const newKeys = Object.keys(newObject);
 
-      if (!finalObject[newKeys[0]]) {
-        finalObject = { ...finalObject, ...newObject };
-      } else {
-        finalObject[newKeys[0]] = {
-          ...finalObject[newKeys[0]],
-          ...newObject[newKeys[0]],
-        };
-      }
-    });
+        if (!finalObject[newKeys[0]]) {
+          finalObject = { ...finalObject, ...newObject };
+        } else {
+          finalObject[newKeys[0]] = {
+            ...finalObject[newKeys[0]],
+            ...newObject[newKeys[0]],
+          };
+        }
+      });
 
-    finalJson = JSON.stringify(finalObject);
+      finalJson = JSON.stringify(finalObject);
+    } catch {
+      invalid = true;
+
+      setTimeout(() => {
+        invalid = false;
+      }, 5000);
+    }
+  }
+
+  async function copyData() {
+    await navigator.clipboard
+      .writeText(finalJson)
+      .then(() => {
+        copied = true;
+
+        setTimeout(() => {
+          copied = false;
+        }, 2000);
+      })
+      .catch(() => {
+        failed = true;
+
+        setTimeout(() => {
+          failed = false;
+        }, 2000);
+      });
   }
 </script>
 
-<main>
-  <textarea on:change={parseJson} />
-  <button on:click={submit}>Parse to object</button>
+<main class="main">
+  <div class="content">
+    <textarea class="textarea" on:keyup={parseJson} />
+    <button
+      class="button"
+      disabled={!unparsed || unparsed === ""}
+      on:click={submit}>Parse to object</button
+    >
 
-  {#if finalJson}
-    {finalJson}
-  {/if}
+    {#if finalJson}
+      <div class="parsed" role="button" on:click={copyData}>
+        {finalJson}
+
+        <div class="message" class:green={copied} class:red={failed}>
+          {#if !copied && !failed}
+            Click to copy
+          {/if}
+
+          {#if copied}
+            Copied!
+          {/if}
+
+          {#if failed}
+            Failed to copy
+          {/if}
+        </div>
+      </div>
+    {/if}
+
+    {#if invalid}
+      <p class="invalid red">Invalid JSON format</p>
+    {/if}
+  </div>
 </main>
+
+<style src="./index.scss"></style>
